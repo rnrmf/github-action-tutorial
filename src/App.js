5f8f3627a50041b8313dcca2379659d854eae2ee
Gibeom Lee <esk8138@naver.com>
2020-02-24 16:08:06 +0900

    v0.02

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. hello
-        </p>
+          React 기반 동적 웹페이지</p><p>
+          AWS S3 웹 호스팅</p><p>
+          <code>
+          -LEEGIBEOM-
+          </code>
+          </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="ahn.html"
           rel="noopener noreferrer"
         >
-          Learn React
+          Next Page
         </a>
       </header>
     </div>
